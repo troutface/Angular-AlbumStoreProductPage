0a147d5b6ad98214abed12a18422873b14639bd0
Greg Harris <Greg-Harris@cox.net>
2021-04-17 15:09:24 -0500

    part6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,10 +5,12 @@ import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -16,4 +18,9 @@ export class ProductService {
     return this._http.get(this._albumUrl)
             .map((response) => <Album> response.json());
   } 
+
+  getProducts(): Observable<Array<Product>> {
+    return this._http.get(this._productsUrl)
+            .map((response) => <Array<Product>> response.json());
+  }
 }

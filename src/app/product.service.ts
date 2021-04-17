b039c776e004657e439b9a7b416866b15ff3e799
Greg Harris <Greg-Harris@cox.net>
2021-04-17 14:13:19 -0500

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/map';
 export class ProductService {
   private _albumUrl = '../assets/album.json';
 
-  constructor(private _http: Http) {}
+  constructor(private _http: Http) { }
 
   getAlbum(id: number) {
     return this._http.get(this._albumUrl)
